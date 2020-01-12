f5b28900767b845c241506eecbf991b94a3f6adc
Jakub Cisło <cubix651@gmail.com>
2020-01-12 12:33:17 +0100

    Simplify routing

--- Github2Wandbox/Startup.cs
+++ Github2Wandbox/Startup.cs
@@ -36,7 +36,7 @@ namespace Github2Wandbox
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -51,7 +51,8 @@ namespace Github2Wandbox
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{action=Index}",
+                    defaults: new { controller = "Home" });
             });
         }
     }
