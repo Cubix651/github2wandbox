ded5f5098e007005f05e9ee4d83ee810ddda9c50
Jakub Cisło <cubix651@gmail.com>
2020-01-25 12:05:20 +0100

    Create publications migration

---
+++ Github2Wandbox/Migrations/20200125110114_publications.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Github2Wandbox.Migrations
+{
+    public partial class publications : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Publications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Owner = table.Column<string>(nullable: true),
+                    Repository = table.Column<string>(nullable: true),
+                    MainPath = table.Column<string>(nullable: true),
+                    CommitSha = table.Column<string>(nullable: true),
+                    Url = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Publications", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Publications");
+        }
+    }
+}
