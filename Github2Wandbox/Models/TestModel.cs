1249f274a319130c3779e02fd1039e89513cc0b0
Jakub Cisło <cubix651@gmail.com>
2020-01-25 11:31:08 +0100

    Remove unnecessary test model

--- Github2Wandbox/Models/TestModel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-namespace Github2Wandbox.Models
-{
-    public class TestModel
-    {
-        public string Option { get; set; }
-        public string Option2 { get; set; }
-    }
-}
