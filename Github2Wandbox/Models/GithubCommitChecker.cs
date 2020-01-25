521b8b7f4c60dfd4f5816508e46db5f62fc1d713
Jakub Cisło <cubix651@gmail.com>
2020-01-25 11:35:48 +0100

    Create GithubCommitChecker class

---
+++ Github2Wandbox/Models/GithubCommitChecker.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Github2Wandbox.Models.Github;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Github2Wandbox.Models
+{
+    public class GithubDirectoryCommitChecker
+    {
+        HttpClient httpClient;
+        JsonSerializerSettings jsonSettings;
+
+        public static string UserAgent { get; } = "Github2Wandbox";
+
+        public GithubDirectoryCommitChecker()
+        {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(UserAgent);
+
+            jsonSettings = new JsonSerializerSettings();
+            jsonSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            jsonSettings.NullValueHandling = NullValueHandling.Ignore;
+        }
+
+        private async Task<string> GetHttpAsync(string url)
+        {
+            var response = await httpClient.GetAsync(url);
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> GetCommitShaAsync(GithubDirectoryDescription description)
+        {
+            int index = description.MainPath.LastIndexOf('/');
+            string mainDirectory = description.MainPath.Substring(0, index);
+            string escapedMainDirectory = WebUtility.UrlEncode(mainDirectory);
+            string apiUrl = $"https://api.github.com/repos/{description.Owner}/" +
+                $"{description.Repository}/commits?path={escapedMainDirectory}";
+            string response = await GetHttpAsync(apiUrl);
+            var commits = JsonConvert.DeserializeObject<List<CommitResponse>>(response, jsonSettings);
+            var commit = commits.First();
+            return commit.Sha;
+        }
+    }
+}
