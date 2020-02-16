085dc552dcea837f151c85daece39d781fad0cca
Jakub Cisło <cubix651@gmail.com>
2020-02-16 01:41:25 +0100

    Add more unit tests to GithubToWandboxTest

--- Github2WandboxUnitTests/GithubToWandboxTest.cs
+++ Github2WandboxUnitTests/GithubToWandboxTest.cs
@@ -75,5 +75,203 @@ namespace Github2WandboxUnitTests
             Assert.Equal("https://wandbox.org/permlink/wandboxHash", url);
         }
 
+        [Fact]
+        public async Task should_return_new_url_when_repo_updated()
+        {
+            var publication = new Publication
+            {
+                CommitSha = "commithash",
+                MainPath = "a/b/c",
+                Owner = "owner",
+                Repository = "repo",
+                Url = "https://wandbox.org/permlink/wandboxHash"
+            };
+            context.Publications.Add(publication);
+            context.SaveChanges();
+
+            var githubDirectoryDescription = new GithubDirectoryDescription
+            {
+                MainPath = "a/b/c",
+                Owner = "owner",
+                Repository = "repo"
+            };
+            A.CallTo(() => githubCommitChecker.GetCommitShaAsync(githubDirectoryDescription))
+                .Returns("commithash2");
+
+            var sourceFiles = A.Fake<SourceFiles>();
+            A.CallTo(() => githubScanner.GetSourceFilesAsync(githubDirectoryDescription))
+                .Returns(sourceFiles);
+
+            var wandboxOptions = A.Fake<WandboxOptions>();
+            A.CallTo(() => wandboxPublisher.PublishAsync(sourceFiles, wandboxOptions))
+                .Returns("https://wandbox.org/permlink/wandboxHash2");
+
+            var transformationDescription = new TransformationDescription
+            {
+                GithubDirectoryDescription = githubDirectoryDescription,
+                WandboxOptions = wandboxOptions
+            };
+
+            var url = await instance.TransformAsync(transformationDescription);
+
+            Assert.Equal("https://wandbox.org/permlink/wandboxHash2", url);
+        }
+
+        [Fact]
+        public async Task should_return_new_url_when_not_in_database()
+        {
+            var githubDirectoryDescription = new GithubDirectoryDescription
+            {
+                MainPath = "a/b/c",
+                Owner = "owner",
+                Repository = "repo"
+            };
+            A.CallTo(() => githubCommitChecker.GetCommitShaAsync(githubDirectoryDescription))
+                .Returns("commithash");
+
+            var sourceFiles = A.Fake<SourceFiles>();
+            A.CallTo(() => githubScanner.GetSourceFilesAsync(githubDirectoryDescription))
+                .Returns(sourceFiles);
+
+            var wandboxOptions = A.Fake<WandboxOptions>();
+            A.CallTo(() => wandboxPublisher.PublishAsync(sourceFiles, wandboxOptions))
+                .Returns("https://wandbox.org/permlink/wandboxHash");
+
+            var transformationDescription = new TransformationDescription
+            {
+                GithubDirectoryDescription = githubDirectoryDescription,
+                WandboxOptions = wandboxOptions
+            };
+
+            var url = await instance.TransformAsync(transformationDescription);
+
+            Assert.Equal("https://wandbox.org/permlink/wandboxHash", url);
+        }
+
+        [Fact]
+        public async Task should_return_new_url_when_main_path_mismatched()
+        {
+            var publication = new Publication
+            {
+                CommitSha = "commithash",
+                MainPath = "a/b/d",
+                Owner = "owner",
+                Repository = "repo",
+                Url = "https://wandbox.org/permlink/wandboxHash"
+            };
+            context.Publications.Add(publication);
+            context.SaveChanges();
+
+            var githubDirectoryDescription = new GithubDirectoryDescription
+            {
+                MainPath = "a/b/c",
+                Owner = "owner",
+                Repository = "repo"
+            };
+            A.CallTo(() => githubCommitChecker.GetCommitShaAsync(githubDirectoryDescription))
+                .Returns("commithash");
+
+            var sourceFiles = A.Fake<SourceFiles>();
+            A.CallTo(() => githubScanner.GetSourceFilesAsync(githubDirectoryDescription))
+                .Returns(sourceFiles);
+
+            var wandboxOptions = A.Fake<WandboxOptions>();
+            A.CallTo(() => wandboxPublisher.PublishAsync(sourceFiles, wandboxOptions))
+                .Returns("https://wandbox.org/permlink/wandboxHash");
+
+            var transformationDescription = new TransformationDescription
+            {
+                GithubDirectoryDescription = githubDirectoryDescription,
+                WandboxOptions = wandboxOptions
+            };
+
+            var url = await instance.TransformAsync(transformationDescription);
+
+            Assert.Equal("https://wandbox.org/permlink/wandboxHash", url);
+        }
+
+        [Fact]
+        public async Task should_return_new_url_when_owner_mismatched()
+        {
+            var publication = new Publication
+            {
+                CommitSha = "commithash",
+                MainPath = "a/b/c",
+                Owner = "owner2",
+                Repository = "repo",
+                Url = "https://wandbox.org/permlink/wandboxHash"
+            };
+            context.Publications.Add(publication);
+            context.SaveChanges();
+
+            var githubDirectoryDescription = new GithubDirectoryDescription
+            {
+                MainPath = "a/b/c",
+                Owner = "owner",
+                Repository = "repo"
+            };
+            A.CallTo(() => githubCommitChecker.GetCommitShaAsync(githubDirectoryDescription))
+                .Returns("commithash");
+
+            var sourceFiles = A.Fake<SourceFiles>();
+            A.CallTo(() => githubScanner.GetSourceFilesAsync(githubDirectoryDescription))
+                .Returns(sourceFiles);
+
+            var wandboxOptions = A.Fake<WandboxOptions>();
+            A.CallTo(() => wandboxPublisher.PublishAsync(sourceFiles, wandboxOptions))
+                .Returns("https://wandbox.org/permlink/wandboxHash");
+
+            var transformationDescription = new TransformationDescription
+            {
+                GithubDirectoryDescription = githubDirectoryDescription,
+                WandboxOptions = wandboxOptions
+            };
+
+            var url = await instance.TransformAsync(transformationDescription);
+
+            Assert.Equal("https://wandbox.org/permlink/wandboxHash", url);
+        }
+
+        [Fact]
+        public async Task should_return_new_url_when_repo_mismatched()
+        {
+            var publication = new Publication
+            {
+                CommitSha = "commithash",
+                MainPath = "a/b/c",
+                Owner = "owner",
+                Repository = "repo2",
+                Url = "https://wandbox.org/permlink/wandboxHash"
+            };
+            context.Publications.Add(publication);
+            context.SaveChanges();
+
+            var githubDirectoryDescription = new GithubDirectoryDescription
+            {
+                MainPath = "a/b/c",
+                Owner = "owner",
+                Repository = "repo"
+            };
+            A.CallTo(() => githubCommitChecker.GetCommitShaAsync(githubDirectoryDescription))
+                .Returns("commithash");
+
+            var sourceFiles = A.Fake<SourceFiles>();
+            A.CallTo(() => githubScanner.GetSourceFilesAsync(githubDirectoryDescription))
+                .Returns(sourceFiles);
+
+            var wandboxOptions = A.Fake<WandboxOptions>();
+            A.CallTo(() => wandboxPublisher.PublishAsync(sourceFiles, wandboxOptions))
+                .Returns("https://wandbox.org/permlink/wandboxHash");
+
+            var transformationDescription = new TransformationDescription
+            {
+                GithubDirectoryDescription = githubDirectoryDescription,
+                WandboxOptions = wandboxOptions
+            };
+
+            var url = await instance.TransformAsync(transformationDescription);
+
+            Assert.Equal("https://wandbox.org/permlink/wandboxHash", url);
+        }
     }
 }
